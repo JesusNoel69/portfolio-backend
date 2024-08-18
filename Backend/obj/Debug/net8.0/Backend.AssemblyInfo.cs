f674e946f18acb96664b1d843347fe8acf025bd6
Jesus <jnoel883@gmail.com>
2024-08-18 10:03:09 -0600

    Ruta de carpeta csproj.

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359d53291a56b51bd8fb426079df5430d2c4372f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1cd0690ad784e2ef691791d192ef39255056d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
